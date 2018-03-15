8a463413451c72245e649c5e73be3412f1f4ec86
swarnk <swarnk@users.noreply.github.com>
2018-03-15 18:30:40 +0300

    fix

--- new-mqtt.ino
+++ new-mqtt.ino
@@ -140,9 +140,8 @@ void loop() {
      //int temperature;
      //temperature = analogRead(15);
      //String myString = String(temperature);
-    char temperature[3];
-      temperature = 23;
-//     Serial.println("SENSOR VALUE:"); Serial.println(temp);
+    char temperature[ ] = 23;
+      //     Serial.println("SENSOR VALUE:"); Serial.println(temp);
        mqtt.publish("/home/water", temperature);
 
     last = millis();
